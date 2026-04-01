bcfa5512deecc140e57e1260f8568180f58082cc
Tejeswar-16 <tejeswarsakthidaran@gmail.com>
2026-04-01 19:13:09 +0530

    added eye button to password field

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,14 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "./_util/config";
 import { useRouter } from "next/navigation";
 import Image from "next/image"
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 export default function Home() {
 
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
   const [loading,setLoading] = useState(false);
+  const [showPassword,setShowPassword] = useState(false);
 
   const router = useRouter();
 
@@ -61,8 +63,16 @@ export default function Home() {
             <div className="flex justify-center">
               <input value={email} onChange={(e) => {setEmail(e.target.value)}} required className="border border-gray-400 placeholder font-sans p-2 rounded-lg my-5 md:my-5 md:w-110 md:h-10" type="email" placeholder="Email"/>
             </div>
-            <div className="flex justify-center">
-              <input value={password} onChange={(e) => {setPassword(e.target.value)}} required className="border border-gray-400 placeholder font-sans p-2 rounded-lg md:w-110 md:h-10" type="password" placeholder="Password"/>
+            <div className="relative">
+              <div className="flex justify-center">
+                <input value={password} 
+                      onChange={(e) => {setPassword(e.target.value)}} 
+                      required 
+                      className="border border-gray-400 placeholder font-sans p-2 rounded-lg md:w-110 md:h-10" 
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"/>
+              </div>
+              <button onClick={() => setShowPassword(!showPassword)} type="button" className="absolute hover:cursor-pointer right-8 top-1/2 -translate-y-1/2 text-xl text-gray-600 hover:text-gray-900">{showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}</button>
             </div>
             <div className="flex justify-center">
               <button type="submit" className={(password !== "") ? "font-sans text-white text-bold bg-black rounded-lg hover:cursor-pointer my-5 w-110 h-10 mx-5 md:text-xl md:my-5 md:w-110 md:h-10" : "font-sans text-white text-bold bg-gray-400 rounded-lg hover:cursor-pointer my-5 w-110 h-10 mx-5 md:text-xl md:my-5 md:w-110 md:h-10"}>Sign In</button>
